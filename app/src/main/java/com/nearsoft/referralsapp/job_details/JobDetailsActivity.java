95ad556f54a4c3d0e27bbe2c12ecafc64b291151
hetovar <htovar@nearsoft.com>
2018-07-05 09:48:33 -0500

    #4 Able to choose a file from local storage and show to the user.

--- app/src/main/java/com/nearsoft/referralsapp/job_details/JobDetailsActivity.java
+++ app/src/main/java/com/nearsoft/referralsapp/job_details/JobDetailsActivity.java
@@ -1,12 +1,19 @@
 package com.nearsoft.referralsapp.job_details;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
+import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.nearsoft.referralsapp.JobDescription;
 import com.nearsoft.referralsapp.R;
@@ -19,6 +26,8 @@ import java.util.List;
 public class JobDetailsActivity extends AppCompatActivity {
     private ArrayList<String> mJobDescription = new ArrayList<>();
     private ArrayList<Integer> mTitle = new ArrayList<>();
+
+    private static final int RESUME_FILE_CODE = 9002;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +52,38 @@ public class JobDetailsActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), ReferralActivity.class));
             }
         });
+
+        ImageButton buttonResumeFile = findViewById(R.id.attach_file_button);
+        buttonResumeFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentGetFile = new Intent(Intent.ACTION_GET_CONTENT);
+                intentGetFile.setType("application/pdf");
+                startActivityForResult(intentGetFile, RESUME_FILE_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case RESUME_FILE_CODE:
+                if(resultCode == RESULT_OK){
+                    Uri returnUri = data.getData();
+
+                    Cursor returnCursor = getContentResolver().query(returnUri, null,
+                            null, null, null);
+                    int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+                    returnCursor.moveToFirst();
+
+                    TextView resumeTextView = findViewById(R.id.resume_text_view);
+                    resumeTextView.setText(returnCursor.getString(nameIndex));
+
+                    ConstraintLayout layout = findViewById(R.id.file_attachment_layout);
+                    layout.setVisibility(View.VISIBLE);
+                }
+        }
     }
 
     private void FillDescription() {
